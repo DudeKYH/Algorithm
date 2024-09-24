c337860efd25813a378bdaf12984b972780f66a6
DudeKYH <ehe151@gmail.com>
2024-09-24 11:35:57 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,5 +1,5 @@
-SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents, date_format(r.created_date, '%Y-%m-%d')
+SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents, date_format(r.created_date, "%Y-%m-%d") AS created_date
 FROM used_goods_board b
-INNER JOIN used_goods_reply r on r.board_id=b.board_id
-WHERE b.created_date LIKE "2022-10%"
-order by r.created_date, b.title
+INNER JOIN used_goods_reply r ON b.board_id=r.board_id
+WHERE date_format(b.created_date, "%Y-%m")="2022-10"
+ORDER BY r.created_date asc, b.title asc;
