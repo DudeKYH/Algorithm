5c6a961c0936933b1a3917b292eda9f37d42351f
DudeKYH <ehe151@gmail.com>
2024-09-24 10:55:39 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2606.81 ms, Memory: 56.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,23 @@
+function sumArr(num, arr, set) {   
+    for(let start = 0; start < arr.length; start++) {
+        let sum = 0;
+        for(let index = 0; index < num; index++) {
+            sum += arr[(start + index) % arr.length];
+        }
+        set.add(sum);
+    }
+}
+
+function solution(elements) {
+    var answer = 0;
+    
+    const sumSet = new Set();
+    
+    for(let i = 1; i <= elements.length; i++) {
+        sumArr(i, elements, sumSet);
+    }
+    
+    answer = sumSet.size;
+    
+    return answer;
+}
