cbe638a69e91a9398d3d4b5f9f33ff5861fb1b7a
DudeKYH <ehe151@gmail.com>
2024-10-29 22:14:45 +0900

    [level 1] Title: 최소직사각형, Time: 29.11 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,16 +1,11 @@
 function solution(sizes) {
     var answer = 0;
     
-    //const sortSizes = sizes.map((size) => size.sort((a, b) => a - b));
-     
-    sizes.forEach((size) => {
-      size.sort((a, b) => a - b);
-    });
+    const sortSizes = sizes.map((size) => size.sort((a, b) => a - b));
     
-    console.log(sizes);
     
-    const maxWidth = Math.max(...sizes.map((size) => size[0]));
-    const maxHeight = Math.max(...sizes.map((size) => size[1]));
+    const maxWidth = Math.max(...sortSizes.map((size) => size[0]));
+    const maxHeight = Math.max(...sortSizes.map((size) => size[1]));
     
     answer = maxWidth * maxHeight;
     return answer;
